d05d23ab4f43cbf8c066c8391a408973d9f622e1
JamesSteff <jamesstefan.legaspino@cit.edu>
2026-09-23 20:31:49 +0800

    init: vite react app

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+}
+
+const App = () => {
+  const course = 'Industry Elective 1'
+  const part1 = 'Industry Elective 2'
+  const exercises1 = 3
+  const part2 = 'Industry Elective 3'
+  const exercises2 = 3
+  const part3 = 'Information Assurance and Security 2'
+  const exercises3 = 3  
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content 
+        part1={part1} exercises1={exercises1} 
+        part2={part2} exercises2={exercises2} 
+        part3={part3} exercises3={exercises3} 
+      />
+      <Total 
+        exercises1={exercises1} 
+        exercises2={exercises2} 
+        exercises3={exercises3} 
+      />
+    </div>
+  )
+}
+
+export default App
